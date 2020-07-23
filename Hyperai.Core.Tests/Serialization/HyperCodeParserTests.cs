78779d1e9ac977c3b31f1efeb6c5f63c5622f63a
ac682 <2419328026@qq.com>
2020-07-24 01:28:47 +0800

    修复HyperCode无法处理url的问题

--- Hyperai.Core.Tests/Serialization/HyperCodeParserTests.cs
+++ Hyperai.Core.Tests/Serialization/HyperCodeParserTests.cs
@@ -16,12 +16,12 @@ namespace Hyperai.Core.Tests.Serialization
             MessageChainBuilder builder = new MessageChainBuilder();
             builder.Add(new Source(1024));
             builder.AddPlain("World");
-            builder.Add(new Image("id", new Uri("https://example.com")));
+            builder.Add(new Image("id", new Uri("https://example.com/a_B-C?a=2&b=3")));
             builder.AddPlain("Hello");
             MessageChain chain = builder.Build();
             HyperCodeParser parser = new HyperCodeParser();
             // A
-            MessageChain res = parser.Parse("[hyper.source(1024)]World[hyper.image(id,https://example.com/)]Hello");
+            MessageChain res = parser.Parse("[hyper.source(1024)]World[hyper.image(id,https://example.com/a_B-C?a=2&b=3)]Hello");
 
 
             // A
