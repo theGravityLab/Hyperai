2e9637e1a88804e93081e28f0bd9a4b2e14151fc
ac682 <2419328026@qq.com>
2020-08-21 21:19:03 +0800

    修改票据访问级

--- Hyperai.Abstractions/Receipts/MessageReceipt.cs
+++ Hyperai.Abstractions/Receipts/MessageReceipt.cs
@@ -6,6 +6,6 @@ namespace Hyperai.Receipts
 {
     public class MessageReceipt : GenericReceipt
     {
-        public long MessageId => (long)this[nameof(MessageId)];
+        public long MessageId { get => (long)this[nameof(MessageId)]; set => this[nameof(MessageId)]= value; }
     }
 }
